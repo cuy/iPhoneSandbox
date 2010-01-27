6c4170e145198aed06b437c36f988d53972ec56e
Charles <cuy@g2ix.com>
2010-01-27 10:51:38 +0800

    * initial checkin with project files

---
+++ Classes/iPhoneSandboxAppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  iPhoneSandboxAppDelegate.h
+//  iPhoneSandbox
+//
+//  Created by Charles Joseph Uy on 1/27/10.
+//  Copyright Home 2010. All rights reserved.
+//
+
+@interface iPhoneSandboxAppDelegate : NSObject <UIApplicationDelegate> {
+
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iPhoneSandbox
+//
+//  Created by Charles Joseph Uy on 1/27/10.
+//  Copyright Home 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
