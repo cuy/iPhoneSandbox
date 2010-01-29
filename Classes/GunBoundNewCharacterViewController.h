af4e24f3f802eb903cc8f08e539665271e4f79e0
rad182 <slackboy182@gmail.com>
2010-01-29 09:29:59 +0800

    added newcharacter class and xib

---
+++ Classes/GunBoundNewCharacterViewController.h
@@ -0,0 +1,16 @@
+//
+//  GunBoundNewCharacterViewController.h
+//  iPhoneSandbox
+//
+//  Created by Royce Dy on 1/28/10.
+//  Copyright 2010 G2iX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GunBoundNewCharacterViewController : UIViewController {
+
+}
+
+@end
