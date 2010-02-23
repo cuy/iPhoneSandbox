253f981a41f38f1bb3945905a3445e0958dd9680
Royce Albert Dy <rdy@g2ix.com>
2010-02-23 17:02:22 +0800

    missile angle testing

--- Classes/Missile.h
+++ Classes/Missile.h
@@ -10,8 +10,24 @@
 #import "Mount.h"
 
 @interface Missile : UIView {
-
+	
+	CGFloat mVelocity;
+	CGFloat mGravity;
+	CGFloat mTime;
+	CGFloat mAngle;
+	
+	NSTimer *mTimer;
+	Mount *mMountView;
 }
+
+@property (readwrite, assign) CGFloat mVelocity;
+@property (readwrite, assign) CGFloat mGravity;
+@property (readwrite, assign) CGFloat mTime;
+@property (readwrite, assign) CGFloat mAngle;
+
+@property(retain, nonatomic) NSTimer *mTimer;
+@property(retain, nonatomic) Mount *mMountView;
+
 
 + (Missile *) initMissile;
 - (void) fireMissileFrom: (Mount *)mountView;
--- Classes/Missile.m
+++ Classes/Missile.m
@@ -12,6 +12,10 @@
 
 @implementation Missile
 
+@synthesize mVelocity,mGravity,mAngle,mTime;
+@synthesize mTimer;
+@synthesize mMountView;
+
 /*
 - (id<CAAction>)actionForLayer:(CALayer *)layer forKey:(NSString *)key {
 	id<CAAction> animation = nil;
@@ -24,6 +28,17 @@
 	return animation;
 }
 */
+/*
+- (id) init
+{
+	[super init];
+	mVelocity = 10.0f;
+	mGravity = 10.0f;
+	mAngle = 1.0f;
+	mTime = 0.0f;
+	return self;
+}
+*/
 
 + (Missile *) initMissile
 {
@@ -31,13 +46,13 @@
 	CGRect rect = CGRectMake(500.0f,0, size, size);
 	Missile *missileView = [[Missile alloc] initWithFrame:rect];
 	missileView.backgroundColor = [UIColor blackColor];
-	missileView.hidden =YES;
+	missileView.hidden = YES;
 	return missileView;
 }
 
 - (void) fireMissileFrom: (Mount *)mountView
 {
-	CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position"];
+/*	CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position"];
     [animation setDelegate:self];
 	[animation setValue:@"fireMissile" forKey:@"name"];
 	[animation setFromValue:[NSValue valueWithCGPoint:mountView.center]];
@@ -51,6 +66,35 @@
     [[self layer] addAnimation:animation forKey:@"fireMissile"];
 	
 	NSLog(@"missile pos y: %f x:%f",mountView.center.y,mountView.center.x);
+*/
+	
+	mVelocity = 0.1f;
+	mGravity = 0.001f;
+	mAngle = 1.0f;
+	mTime = 0.0f;
+	
+	mTimer = [NSTimer scheduledTimerWithTimeInterval:1.0f/30.0f target:self selector:@selector(startFireMissile) userInfo:nil repeats:YES];	
+	//[self startFireMissile];
+}
+
+- (void) startFireMissile
+{
+	
+	CGPoint pos = self.center;
+	NSLog(@"velocity: %f gravity: %f angle: %f",mVelocity,mGravity,mAngle);
+	//pos.x = 100.0f;
+	//pos.y = 100.0f;
+	pos.x = mVelocity * mTime * cos(mAngle * (M_PI/180)) + self.center.x;
+	pos.y = (mGravity * (mTime * mTime)) - ((mVelocity * mTime) * sin(mAngle * (M_PI/180))) + self.center.y;	
+	NSLog(@"pos.x %f pos.y %f mtime: %f",pos.x,pos.y,mTime);
+	self.center = pos;
+	
+	//mTime += 1.0f/30.f;
+	mTime +=1.0f;
+	if (mTime >= 3) {
+		[mTimer invalidate];
+		mTimer = nil;
+	}
 	
 }
 
