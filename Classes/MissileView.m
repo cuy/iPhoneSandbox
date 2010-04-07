e88f88e3746509ca6b0ecae08dca180b9ded6db2
Charles <cuy@g2ix.com>
2010-04-07 14:29:07 +0800

    * initial restructure for missleview

--- Classes/MissileView.m
+++ Classes/MissileView.m
@@ -49,31 +49,13 @@
 	mMountView = mountView;
 	mEnemyMountView = enemyMountView;
     
-	//cx = mMountView.center.x;
-	//cy = mMountView.center.y;
-	
-	/*/ set missile starting point
-	//CGPoint mPos = mMountView.center;
-	if (mMountView.mMount.player == 1) {
-		cx += 30;
-		cy += 5;
-	}
-	else {
-		cx -= 30;
-		cy += 5;
-	}*/
-	
-	//mMissile.position = mPos;
-	//NSLog(@"angle: %f", mMissile.angle);
-	
+    NSLog(@"mMissle(x,y) = (%f,%f)", mMissile.cx, mMissile.cy);
+    // cx and cy are muzzle view's center
 	position.x = cx = mMissile.cx + cos(mMissile.angle * M_PI/180); 
 	position.y = cy = mMissile.cy + sin(mMissile.angle * M_PI/180);
-	//mMissile.position = pos;
 	self.center = position;
-	// rotate missile with given angle
-	CGFloat ang = mMissile.angle*-1;  //atan2(position.y - oposition.y, position.x - oposition.x)*180.0/M_PI;
+	CGFloat ang = mMissile.angle*-1;
 	self.transform = CGAffineTransformMakeRotation(ang* M_PI/180);
-	//NSLog(@"cx[ %f ] cy[ %f ]", mMissile.cx, mMissile.cy);
 	
 	oposition.x = mMissile.cx;
 	oposition.y = mMissile.cy;
@@ -81,26 +63,22 @@
 	mMissile.gravity = 20.0;
 	mMissile.time = 0;
 	mTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 / 60.0  target:self selector:@selector(startFireMissile) userInfo:nil repeats:YES];	
-
 }
 
 - (void)startFireMissile {
 	// launch missile
 	mMissile.time += 1.0/10;
+    // x = xo + v * cos(angle) * t
 	position.x = mMissile.velocity * mMissile.time * cos(mMissile.angle * M_PI/180) + cx;
+    // y = yo + 0.5 * g * t * t - v * sin(angle) * t
 	position.y = 0.5 * mMissile.gravity * mMissile.time * mMissile.time - mMissile.velocity * mMissile.time * sin((mMissile.angle * M_PI)/180) + cy;	
 	self.center = position;
-	//NSLog(@"Missile Angle = %f",rot);
-	//NSLog(@"cx = %f, cy = %f", mMissile.position.x, mMissile.position.y);
+    // rotate the missle to match the trajectory
 	CGFloat rot = atan2(position.y - oposition.y, position.x - oposition.x)*180.0/M_PI;
 	self.transform = CGAffineTransformMakeRotation(round(rot)*M_PI/180);
 	oposition = self.center;
-	NSLog(@"px[%f] ox[%f] py[%f] oy[%f]", self.center.x, oposition.x, self.center.y, oposition.y);
-	NSLog(@"angle: %f",rot);
 	
-	//[self setNeedsDisplay];
-	
-	//NSLog(@"missile position x: %f y: %f",mMissile.position.x,mMissile.position.y);
+    // check if out of screen or hit the enemy mount
 	if ((self.center.y >= 320.0f  || self.center.x >= 480.0f) || [self didHitEnemyMountView]) {
 		[mTimer invalidate];
 		mTimer = nil;
@@ -114,7 +92,6 @@
 
 - (BOOL)didHitEnemyMountView {
 	if (CGRectIntersectsRect(self.frame, mEnemyMountView.frame)) {
-		NSLog(@"naka igo ka brad!");
 		return YES;
 	}
 	else {
@@ -127,6 +104,5 @@
 	[mMissileImageView release];
 	[mMissile release];
 }
-
 
 @end
